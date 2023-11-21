41010031b8a4c735471aa3550b9563e0ef2af492
19983467897 <2715158815.com>
2023-11-21 22:28:30 +0800

    feat: 乱开发

--- app/404.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-	return (
-		<>
-			<span className="text-white">404</span>
-		</>
-	)
-}
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function LoadingPage() {
+	return (
+		<main className="flex min-h-screen w-full flex-col items-center p-8 text-white">
+			<h1 className="text-lg font-bold">Loading</h1>
+		</main>
+	)
+}
